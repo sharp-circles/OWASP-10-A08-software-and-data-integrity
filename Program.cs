d6f7a8f4eadb0421c87109e41a16b124aa33b748
sharp-circles <sharpcirclesorg@gmail.com>
2025-10-26 17:39:34 +0100

    Initial console app with some of the logging

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("******************************************");
+Console.WriteLine("***** Unsafe deserialization in .NET *****");
+Console.WriteLine("******************************************");
+Console.WriteLine();
+
+Console.WriteLine(" -- Use case 1 -- [ClaimIdentity - BinaryFormatter]");
+Console.WriteLine();
+Console.WriteLine("Starting use case 1");
+Console.WriteLine();
+Console.WriteLine("[Summary]");
+Console.WriteLine("Gadget chain: ClaimIdentity");
+Console.WriteLine("Formatter: BinaryFormatter");
+Console.WriteLine("Command: ");
+Console.WriteLine("Serialized output:");
+
+// Try catch block here
+
+Console.WriteLine("Use case 1 completed");
+Console.WriteLine();
+
+Console.WriteLine(" -- Use case 2 -- [ClaimPrincipal - BinaryFormatter]");
+
+Console.WriteLine();
+Console.WriteLine("[Summary]");
+Console.WriteLine();
+Console.WriteLine("Gadget chain: ClaimPrincipal");
+Console.WriteLine("Formatter: BinaryFormatter");
+Console.WriteLine("Command: ");
+Console.WriteLine("Serialized output:");
+
+// Try catch block here
+
+Console.WriteLine("Use case 2 completed");
+Console.WriteLine();
+
+
+
+
+
